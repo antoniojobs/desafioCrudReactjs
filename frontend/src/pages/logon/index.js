5af74fd34d1ed5f4e3fac1727dac6473a55f1f82
José Antonio <58563869+antoniojobs@users.noreply.github.com>
2020-11-26 19:02:06 -0300

    front logon concluido, global e styles(css)update

--- frontend/src/pages/logon/index.js
+++ frontend/src/pages/logon/index.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import {FiLogIn} from 'react-icons/fi';
+import {FiLogIn,FiArrowDownCircle} from 'react-icons/fi';
 import './styles.css';
-import logonImg from '../../assets/logo.png';
+// import logonImg from '../../assets/logo.png';
 import recruImg from '../../assets/recruiters.png';
 export default function logon() {
     return(
         <div className="logon-container">
-
-        <h1>Ola Brasil</h1>
-        <section className="form">
-            <img src={logonImg} alt='logo'/>
-            <form>
-                <h1>Logue-se aqui</h1>
-                <input placeholder="email"/>
-                <input placeholder="senha"/>
-                <button type="submit">Entrar</button>
-                <a href="/register">
-                    <FiLogIn size={16} color="#ec0e0e" />
-                    Não tenho cadastro
-                </a>
-            </form>
-        </section>
-            <img src={recruImg}alt='recrutadores'/>
+            <section className="form">
+            <FiArrowDownCircle size={100} color="#ec0e0e" />
+                <form>
+                    <h1>Logue-se aqui</h1>
+                    <input placeholder="email"/>
+                    <input placeholder="senha"/>
+                    
+                    <button className="button" type="submit">Entrar</button>
+                    <a className="a" href="/register">
+                        <FiLogIn className="svg" size={16} color="#ec0e0e" />
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+                <img  src={recruImg}alt='recrutadores'/>
         </div>
     );
 }
